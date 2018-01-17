e0b8e5f5ec23c5367c58e48d93d05e0cb4afcf9b
Unknown <lucille.moise@gmail.com>
2018-01-18 00:20:57 +0100

    Changement pc

--- structure/headers/expr.hh
+++ structure/headers/expr.hh
@@ -23,41 +23,41 @@ class Expression : public Term {
     virtual Expression* operator > (const Expression& val)const = 0;
     virtual Expression* operator >=(const Expression& val)const = 0;
 
-    virtual Expression* plus(const IntValue& val)const = 0;
-    virtual Expression* plus(const FloatValue& val)const = 0;
-    virtual Expression* plus(const BoolValue& val)const = 0;
+    virtual Expression* plus(const Integer& val)const = 0;
+    virtual Expression* plus(const Float& val)const = 0;
+    virtual Expression* plus(const Boolean& val)const = 0;
 
-    virtual Expression* moins(const IntValue& val)const = 0;
-    virtual Expression* moins(const FloatValue& val)const = 0;
-    virtual Expression* moins(const BoolValue& val)const = 0;
+    virtual Expression* moins(const Integer& val)const = 0;
+    virtual Expression* moins(const Float& val)const = 0;
+    virtual Expression* moins(const Boolean& val)const = 0;
 
-    virtual Expression* mult(const IntValue& val)const = 0;
-    virtual Expression* mult(const FloatValue& val)const = 0;
-    virtual Expression* mult(const BoolValue& val)const = 0;
+    virtual Expression* mult(const Integer& val)const = 0;
+    virtual Expression* mult(const Float& val)const = 0;
+    virtual Expression* mult(const Boolean& val)const = 0;
 
-    virtual Expression* div(const IntValue& val)const = 0;
-    virtual Expression* div(const FloatValue& val)const = 0;
-    virtual Expression* div(const BoolValue& val)const = 0;
-
-
-    virtual Expression* inferieur(const IntValue& val)const = 0;
-    virtual Expression* inferieur(const FloatValue& val)const = 0;
-    virtual Expression* inferieur(const BoolValue& val)const = 0;
+    virtual Expression* div(const Integer& val)const = 0;
+    virtual Expression* div(const Float& val)const = 0;
+    virtual Expression* div(const Boolean& val)const = 0;
 
 
-    virtual Expression* inferieurEgal(const IntValue& val)const = 0;
-    virtual Expression* inferieurEgal(const FloatValue& val)const = 0;
-    virtual Expression* inferieurEgal(const BoolValue& val)const = 0;
+    virtual Expression* inferieur(const Integer& val)const = 0;
+    virtual Expression* inferieur(const Float& val)const = 0;
+    virtual Expression* inferieur(const Boolean& val)const = 0;
 
 
-    virtual Expression* superieur (const IntValue& val)const = 0;
-    virtual Expression* superieur (const FloatValue& val)const = 0;
-    virtual Expression* superieur (const BoolValue& val)const = 0;
+    virtual Expression* inferieurEgal(const Integer& val)const = 0;
+    virtual Expression* inferieurEgal(const Float& val)const = 0;
+    virtual Expression* inferieurEgal(const Boolean& val)const = 0;
 
 
-    virtual Expression* superieurEgal(const IntValue& val)const = 0;
-    virtual Expression* superieurEgal(const FloatValue& val)const = 0;
-    virtual Expression* superieurEgal(const BoolValue& val)const = 0;*/
+    virtual Expression* superieur (const Integer& val)const = 0;
+    virtual Expression* superieur (const Float& val)const = 0;
+    virtual Expression* superieur (const Boolean& val)const = 0;
+
+
+    virtual Expression* superieurEgal(const Integer& val)const = 0;
+    virtual Expression* superieurEgal(const Float& val)const = 0;
+    virtual Expression* superieurEgal(const Boolean& val)const = 0;*/
 
 };
 

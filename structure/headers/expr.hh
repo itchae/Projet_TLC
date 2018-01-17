f3602832c0f99b90d68cd5a20d50ec472bc92b71
Unknown <lucille.moise@gmail.com>
2018-01-17 17:37:52 +0100

    Ajout opérateurs

--- structure/headers/expr.hh
+++ structure/headers/expr.hh
@@ -11,6 +11,54 @@ class Expression : public Term {
   public:
     ~Expression() { }
     virtual string getType() const = 0;
+
+
+    virtual Expression* operator +(const Expression& val)const = 0;
+    virtual Expression* operator -(const Expression& val)const = 0;/
+    virtual Expression* operator * (const Expression& val)const = 0;
+    virtual Expression* operator /(const Expression& val)const = 0;
+
+    virtual Expression* operator < (const Expression& val)const = 0;
+    virtual Expression* operator <=(const Expression& val)const = 0;/
+    virtual Expression* operator > (const Expression& val)const = 0;
+    virtual Expression* operator >=(const Expression& val)const = 0;
+
+    virtual Expression* plus(const IntValue& val)const = 0;
+    virtual Expression* plus(const FloatValue& val)const = 0;
+    virtual Expression* plus(const BoolValue& val)const = 0;
+
+    virtual Expression* moins(const IntValue& val)const = 0;
+    virtual Expression* moins(const FloatValue& val)const = 0;
+    virtual Expression* moins(const BoolValue& val)const = 0;
+
+    virtual Expression* mult(const IntValue& val)const = 0;
+    virtual Expression* mult(const FloatValue& val)const = 0;
+    virtual Expression* mult(const BoolValue& val)const = 0;
+
+    virtual Expression* div(const IntValue& val)const = 0;
+    virtual Expression* div(const FloatValue& val)const = 0;
+    virtual Expression* div(const BoolValue& val)const = 0;
+
+
+    virtual Expression* inferieur(const IntValue& val)const = 0;
+    virtual Expression* inferieur(const FloatValue& val)const = 0;
+    virtual Expression* inferieur(const BoolValue& val)const = 0;
+
+
+    virtual Expression* inferieurEgal(const IntValue& val)const = 0;
+    virtual Expression* inferieurEgal(const FloatValue& val)const = 0;
+    virtual Expression* inferieurEgal(const BoolValue& val)const = 0;
+
+
+    virtual Expression* superieur (const IntValue& val)const = 0;
+    virtual Expression* superieur (const FloatValue& val)const = 0;
+    virtual Expression* superieur (const BoolValue& val)const = 0;
+
+
+    virtual Expression* superieurEgal(const IntValue& val)const = 0;
+    virtual Expression* superieurEgal(const FloatValue& val)const = 0;
+    virtual Expression* superieurEgal(const BoolValue& val)const = 0;
+
 };
 
 #endif
